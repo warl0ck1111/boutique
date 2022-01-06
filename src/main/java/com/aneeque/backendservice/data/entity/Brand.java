00c02da276fa7006d4bcfe2a9cca78f8b263bb20
Okala III <bashirokala@gmail.com>
2022-01-07 00:39:31 +0100

    added brand service implemetations

---
+++ src/main/java/com/aneeque/backendservice/data/entity/Brand.java
@@ -0,0 +1,35 @@
+package com.aneeque.backendservice.data.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * @author Okala Bashir .O.
+ */
+
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(updatable = false, nullable = false)
+    @CreationTimestamp
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd h:m:s")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd h:m:s")
+    private LocalDateTime modifiedAt;
+}
---
+++ src/main/java/com/aneeque/backendservice/dto/response/BrandResponseDto.java
@@ -0,0 +1,16 @@
+package com.aneeque.backendservice.dto.response;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Okala Bashir .O.
+ */
+
+@Data
+@NoArgsConstructor
+public class BrandResponseDto {
+    private Long id;
+
+    private String name;
+}
