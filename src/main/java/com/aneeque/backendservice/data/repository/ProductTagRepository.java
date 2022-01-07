a3c4393f1451d527ac04daa35a3b088566fd53fa
Okala III <bashirokala@gmail.com>
2022-01-07 01:57:13 +0100

    added product tag service implementation

---
+++ src/main/java/com/aneeque/backendservice/data/repository/ProductTagRepository.java
@@ -0,0 +1,20 @@
+package com.aneeque.backendservice.data.repository;
+
+import com.aneeque.backendservice.data.entity.ProductTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Okala Bashir .O.
+ */
+
+@Repository
+public interface ProductTagRepository extends JpaRepository<ProductTag, Long> {
+
+    @Modifying
+    @Query(value = "DELETE from product_tag  where product_id = :productId AND tag_id = :tagId ", nativeQuery = true)
+    Integer deleteProductTag(@Param("productId") Long productId, @Param("tagId") Long tagId);
+}
