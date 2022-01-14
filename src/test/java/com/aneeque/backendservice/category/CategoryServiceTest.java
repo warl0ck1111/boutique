660c3d35a075f7927e7d8e50cf7331abc9a1c381
Okala III <bashirokala@gmail.com>
2022-01-14 07:32:01 +0100

    updated category servce unit test

--- src/test/java/com/aneeque/backendservice/category/CategoryServiceTest.java
+++ src/test/java/com/aneeque/backendservice/category/CategoryServiceTest.java
@@ -1,29 +1,18 @@
 package com.aneeque.backendservice.category;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.atLeast;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.when;
 
 import com.aneeque.backendservice.data.entity.Attribute;
 import com.aneeque.backendservice.data.entity.Category;
 import com.aneeque.backendservice.data.repository.CategoryRepository;
-import com.aneeque.backendservice.dto.request.AttributeDto;
-import com.aneeque.backendservice.dto.request.CategoryDto;
+import com.aneeque.backendservice.dto.request.CategoryRequestDto;
+import com.aneeque.backendservice.enums.CategoryType;
 import com.aneeque.backendservice.service.CategoryService;
 import com.aneeque.backendservice.service.impl.AttributeServiceImpl;
-import com.aneeque.backendservice.data.entity.Property;
-import com.aneeque.backendservice.dto.request.PropertyDto;
 import com.aneeque.backendservice.service.PropertyService;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -31,6 +20,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @ContextConfiguration(classes = {CategoryService.class})
 @ExtendWith(SpringExtension.class)
@@ -47,9 +39,64 @@ class CategoryServiceTest {
     @MockBean
     private PropertyService propertyService;
 
+
     @Test
-    void testSave() {
-        assertThrows(IllegalArgumentException.class, () -> this.categoryService.save(new CategoryDto()));
+    void testGetAll() {
+        when(this.categoryRepository.findAll()).thenReturn(new ArrayList<Category>());
+        assertTrue(this.categoryService.getAll().isEmpty());
+        verify(this.categoryRepository).findAll();
+        assertTrue(this.categoryService.getAllParent().isEmpty());
+    }
+
+    @Test
+    void testGetAll2() {
+        Category category = new Category();
+        category.setAttributes(new ArrayList<Attribute>());
+        category.setModifiedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        category.setSubCategories(new ArrayList<Category>());
+        category.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        category.setId(123L);
+        category.setName("Name");
+        category.setCategoryType(CategoryType.DEPARTMENT);
+        category.setDescription("The characteristics of someone or something");
+
+        ArrayList<Category> categoryList = new ArrayList<Category>();
+        categoryList.add(category);
+        when(this.categoryRepository.findAll()).thenReturn(categoryList);
+        assertEquals(1, this.categoryService.getAll().size());
+        verify(this.categoryRepository).findAll();
+        assertTrue(this.categoryService.getAllParent().isEmpty());
+    }
+
+    @Test
+    void testGetAll3() {
+        Category category = new Category();
+        category.setAttributes(new ArrayList<Attribute>());
+        category.setModifiedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        category.setSubCategories(new ArrayList<Category>());
+        category.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        category.setId(123L);
+        category.setName("Name");
+        category.setCategoryType(CategoryType.DEPARTMENT);
+        category.setDescription("The characteristics of someone or something");
+
+        Category category1 = new Category();
+        category1.setAttributes(new ArrayList<Attribute>());
+        category1.setModifiedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        category1.setSubCategories(new ArrayList<Category>());
+        category1.setCreatedAt(LocalDateTime.of(1, 1, 1, 1, 1));
+        category1.setId(123L);
+        category1.setName("Name");
+        category1.setCategoryType(CategoryType.DEPARTMENT);
+        category1.setDescription("The characteristics of someone or something");
+
+        ArrayList<Category> categoryList = new ArrayList<Category>();
+        categoryList.add(category1);
+        categoryList.add(category);
+        when(this.categoryRepository.findAll()).thenReturn(categoryList);
+        assertEquals(2, this.categoryService.getAll().size());
+        verify(this.categoryRepository).findAll();
+        assertTrue(this.categoryService.getAllParent().isEmpty());
     }
 
 
