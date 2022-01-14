8d9e2bedc86e250a21e81c8a1182b32661c99586
Okala III <bashirokala@gmail.com>
2022-01-14 03:01:38 +0100

    implemented file upload and download service

---
+++ src/main/java/com/aneeque/backendservice/controller/DocumentController.java
@@ -0,0 +1,52 @@
+package com.aneeque.backendservice.controller;
+
+import com.aneeque.backendservice.dto.response.DocumentResponseDto;
+import com.aneeque.backendservice.dto.response.ApiResponse;
+import com.aneeque.backendservice.service.DocumentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import java.io.IOException;
+
+/**
+ * @author Bashir .O. Okala
+ */
+
+
+@RestController
+@RequestMapping("/api/v1/files")
+public class DocumentController extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private DocumentService documentService;
+
+
+    @Value("{spring.http.multipart.max-file-size}")
+    private String maxfileSize;
+
+    @GetMapping(value = "{fileName:.+}", produces = MediaType.ALL_VALUE)
+    public ResponseEntity<byte[]> getFile(@PathVariable(name = "fileName") String fileName) throws IOException {
+        byte[] media = this.documentService.getImageWithMediaType(fileName);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setCacheControl(CacheControl.noCache().getHeaderValue());
+
+        return new ResponseEntity<>(media, headers, HttpStatus.OK);
+    }
+
+
+
+    @Transactional
+    @PostMapping(path = "upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<?> uploadFile(
+            @RequestPart("fileUpload") MultipartFile file
+    ) {
+        logger.info(file);
+        logger.info("this is the file size" + file.getSize());
+        DocumentResponseDto responseDto = documentService.uploadFile(file);
+        return ResponseEntity.ok(new ApiResponse("file uploaded successfully", responseDto));
+    }
+}
---
+++ src/main/java/com/aneeque/backendservice/dto/response/DocumentResponseDto.java
@@ -0,0 +1,18 @@
+package com.aneeque.backendservice.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Okala Bashir .O.
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DocumentResponseDto {
+
+    private String fileName;
+
+}
