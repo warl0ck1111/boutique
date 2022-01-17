460725fad2245c7be605a5ff67b387e340005df2
Okala III <bashirokala@gmail.com>
2022-01-17 11:37:57 +0100

    modified product image request model

--- src/main/java/com/aneeque/backendservice/dto/request/ProductImageDto.java
+++ src/main/java/com/aneeque/backendservice/dto/request/ProductImageDto.java
@@ -1,14 +1,20 @@
 package com.aneeque.backendservice.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author Okala Bashir .O.
  */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductImageDto {
 
     private String fileName;
 
-    private String url;
-    
     private Long productId;
     
 
